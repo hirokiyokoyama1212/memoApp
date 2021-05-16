bbfac0a7206ef5a33e1ec0aaf60c2a34a34a015e
EC2 Default User <ec2-user@ip-172-31-89-160.ec2.internal>
2021-05-16 04:41:48 +0000

    break error  fix

--- memo.rb
+++ memo.rb
@@ -5,31 +5,31 @@ puts "1(新規でメモを作成) 2(既存のメモ編集する)"
 memo_type = gets.to_i
 
 if memo_type == 1
-	puts "拡張子を除いたファイル名を入力してください"
+  puts "拡張子を除いたファイル名を入力してください"
 	
-	file_name = gets
+  file_name = gets.chomp
 	
-	puts "メモしたい内容を記入してください"
-	puts "完了したらCtrl＋Dを押下してください"
+  puts "メモしたい内容を記入してください"
+  puts "完了したらCtrl＋Dを押下してください"
 	
-	memo = gets
+  memo = gets.chomp
 	
-	CSV.open("#{file_name}.csv","w") do |write|
-	  write << ["#{memo}"]
+  CSV.open("#{file_name}.csv","w") do |write|
+    write << ["#{memo}"]
   end
 
 elsif memo_type == 2
   puts "拡張子を除いたファイル名を入力してください"
   
-  file_name = gets  
+  file_name = gets.chomp
   
   puts "追記したい内容を記入してください"
-  memo = gets
+  memo = gets.chomp
   
   CSV.open("#{file_name}.csv","a") do |write|
     write << ["#{memo}"]
   end	
 
 else
- 	puts "1 or 2を入力してください"
+  puts "1 or 2を入力してください"
 end
